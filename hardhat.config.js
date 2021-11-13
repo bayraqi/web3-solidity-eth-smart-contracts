4a021dec4a249633c9bd54c32060078519b44ccf
bayraqi <bayraqi@outlook.com>
2021-11-13 18:02:24 +0200

    Updated harhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -13,6 +13,3 @@ module.exports = {
 
 
 
-// Deploying contracts with account:  0xfC2Fcd9F872c69F0Ef035aAed726e89dB391cA05
-// Account balance:  25000000000000000
-// WavePortal address:  0x4EBa20627D992B4487f8e25E1d43821Bde076Bd3
